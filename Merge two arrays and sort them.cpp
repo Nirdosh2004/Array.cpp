f4ec2c6b5bb59249f97a08f3d2b859f0663b3ee9
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-05-02 13:46:30 +0530

    Update Merge two arrays and sort them.cpp

--- Merge two arrays and sort them.cpp
+++ Merge two arrays and sort them.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     int n1;
-    cout<<"Enter size of n1: ";
+    cout<<"Enter size of n1: ";   
     cin>>n1;
     int one[n1];
     cout<<"Enter elements of first array : ";
