ca91d2c47beead8ec41ba853175f66039aeb50c6
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-05-08 13:25:44 +0530

    Update diagonal sum in an array.cpp

--- diagonal sum in an array.cpp
+++ diagonal sum in an array.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){    
     
     int matrix[3][3];
-    cout<<"Enter elements of matrix : "<<endl;
+    cout<<"Enter elements of matrix : "<<endl;   
     int i,j;  
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
